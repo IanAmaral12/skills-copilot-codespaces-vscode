513464533bd8991d19877df6bde1ee0e8f626cd3
Ian Amaral <143959606+IanAmaral12@users.noreply.github.com>
2024-10-12 03:35:10 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember(){
+    document.getElementById("skills").style.display = "block";
+    document.getElementById("member").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+}
